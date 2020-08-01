3202eeb70958b0197fc320d08c4d3ec949bc5724
2z1c <1073355312@qq.com>
2020-08-01 20:33:25 +0800

    feat():现在可以生成静态库，并且可以将其复制到对应的发布文件了
    Change-Id: Ifb9917ed1d0ee32715146d8d0e1cfa0c981bef24

---
+++ libsrc/test/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void zApueTestfun(void)
+{
+  printf("current funcation is %s\n", __FUNCTION__);
+  printf("current line is %d\n", __LINE__);
+}
+
+void zApueTestfun1(void)
+{
+  printf("current funcation is %s\n", __FUNCTION__);
+  printf("current line is %d\n", __LINE__);
+}
---
+++ libsrc/test/test.h
@@ -0,0 +1,4 @@
+#ifndef __TEST__H
+void zApueTestfun(void);
+void zApueTestfun1(void); //测试使用
+#endif /*!__TEST__H*/

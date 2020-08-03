2549368bbd1e83175e0362368d284d4c2c7dbc57
2z1c <1073355312@qq.com>
2020-08-03 22:53:40 +0800

    style(): 仅仅只是用了pritnf输出信息
    Change-Id: Ia188ba13d29fe3fda2ad087cb118fdddd963d6e4

--- temp/main.cpp
+++ temp/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 #include "test.h"
 using namespace std;
 int main()
 {
-    cout << "Hello, world!!!!!!!" << endl;
+    printf("Hello, world!!!!!!!\n");
     zApueTestfun1();
     return 0;
 }
